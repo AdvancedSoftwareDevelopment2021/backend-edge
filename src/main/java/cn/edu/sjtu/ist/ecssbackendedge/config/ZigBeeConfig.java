edb38b69f2eaaeb8db4adb22456996b2edf4386b
Dyanjun <dyanjun15201981339@163.com>
2021-11-21 20:14:31 +0800

    feat:zigbee数据采集类

--- src/main/java/cn/edu/sjtu/ist/ecssbackendedge/config/ZigBeeConfig.java
+++ src/main/java/cn/edu/sjtu/ist/ecssbackendedge/config/ZigBeeConfig.java
@@ -1,8 +1,73 @@
 package cn.edu.sjtu.ist.ecssbackendedge.config;
 
+import cn.edu.sjtu.ist.ecssbackendedge.dao.SensorDao;
+import cn.edu.sjtu.ist.ecssbackendedge.model.sensor.Sensor;
+import cn.edu.sjtu.ist.ecssbackendedge.utils.collect.zigbee.ZigBeeListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author dyanjun
  * @date 2021/11/10 23:55
  */
+@Configuration
+@Slf4j
 public class ZigBeeConfig {
+    @Autowired
+    @Qualifier("zigBeeListenerHashMap")
+    private ConcurrentHashMap<String, ZigBeeListener> listenerMap;
+
+    @Bean
+    public ConcurrentHashMap<String, ZigBeeListener> zigBeeListenerHashMap() {
+        return new ConcurrentHashMap<>();
+    }
+
+    @Autowired
+    private SensorDao sensorDao;
+
+
+    /**
+     * @Title getListener
+     * @Description: 通过id获取对应的ZigBeeListener
+     * @params: [id,serialNumber,baudRate,checkoutBit,dataBit,stopBit]
+     * @return: ZigBeeListener
+     */
+    public ZigBeeListener getListener(String id, String serialNumber,int baudRate, int checkoutBit, int dataBit, int stopBit) {
+
+        ZigBeeListener listener = listenerMap.get(id);
+        if(listener == null) {
+            setListener(id, serialNumber, baudRate, checkoutBit, dataBit, stopBit);
+            listener = listenerMap.get(id);
+        }
+
+        return listener;
+
+    }
+
+    /**
+     * @Title setListener
+     * @Description: 设置id对应的ZigBeeListener
+     * @params [id, serialNumber, baudRate, checkoutBit, dataBit, stopBit]
+     * @return void
+     */
+    private void setListener(String id, String serialNumber,int baudRate, int checkoutBit, int dataBit, int stopBit){
+        Sensor sensor = sensorDao.findSensorById(id);
+        ZigBeeListener listener = new ZigBeeListener(sensor);
+        listener.init(serialNumber, baudRate, checkoutBit, dataBit, stopBit);
+        listenerMap.put(id, listener);
+    }
+
+    public void deleteListener(String id){
+        ZigBeeListener listener = listenerMap.get(id);
+        if(listener == null) {
+            return;
+        }
+        listener.closeSerialPort();
+        listenerMap.remove(id);
+    }
 }
